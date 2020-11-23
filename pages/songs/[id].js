884073bba1f069c440a34798690f2f1e365e6ace
axelobscura <axosar@gmail.com>
2020-11-23 14:23:24 -0600

    Testing db

--- pages/songs/[id].js
+++ pages/songs/[id].js
@@ -1,33 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+
 export async function getStaticProps({ params }) {
+  const prisma = new PrismaClient();
+  const song = await prisma.song.findOne({
+    include: { artist: true },
+    where: {
+      id: Number(params.id)
+    }
+  });
+
   return {
     props: {
-      song: {
-        youtubeId: 'N6SQ9QoSjCI'
-      }
+      song
     }
   };
 }
 
 export async function getStaticPaths() {
+  const prisma = new PrismaClient();
+  const songs = await prisma.song.findMany();
+
   return {
-    paths: [
-      {
-        params: {
-          id: '1'
-        }
+    paths: songs.map((song) => ({
+      params: {
+        id: song.id.toString()
       }
-    ],
+    })),
     fallback: false
   };
 }
 
 export default ({ song }) => (
-  <iframe
-    width="100%"
-    height="315"
-    src={`https://www.youtube.com/embed/${song.youtubeId}`}
-    frameBorder="0"
-    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-    allowFullScreen
-  />
+  <p>{song.name}</p>
 );
